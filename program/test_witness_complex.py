Distance calculation bug fixed, some tests added

program/test_witness_complex.py
@@ -13,6 +13,7 @@ TEST_LABELS = [0, 1, 1, 0, 1, 0]
 TEST_CLASSES = range(2)
 TEST_SAMPLES = [np.array([1.75, 1.75]), np.array([1. , 1.5]), np.array([1.25, 0.  ])]
 
+
 class TestWitnessComplex(unittest.TestCase):
 
     def setUp(self):
@@ -24,13 +25,28 @@ class TestWitnessComplex(unittest.TestCase):
         self.sut = WitnessComplexCreator(stub_data_processor, len(TEST_SAMPLES))
 
     @patch('uniform_sampler.UniformSampler')
-    def test_dummy(self, MockUniformSampler):
+    def test_knn_graph_creation(self, MockUniformSampler):
         mock_uniform_sampler = MockUniformSampler.return_value
         expected_nodes = TEST_SAMPLES
         mock_uniform_sampler.sample.return_value = expected_nodes
-        nodes = self.sut.create_knn_graph()
 
 
+
+    @patch('uniform_sampler.UniformSampler')
+    def test_final_adjacency_dict(self, MockUniformSampler):
+        mock_uniform_sampler = MockUniformSampler.return_value
+        expected_nodes = TEST_SAMPLES
+        mock_uniform_sampler.sample.return_value = expected_nodes
+
+        adjacency_dict = self.sut.create_knn_graph()
+        expected_adjacency_dict = {'[1.75 1.75]': [np.array([1. , 1.5]), np.array([1.25, 0.  ])],
+                                   '[1.  1.5]': [np.array([1.75, 1.75]), np.array([1.25, 0.  ])],
+                                   '[1.25 0.  ]': [np.array([1. , 1.5]), np.array([1.75, 1.75])]}
+
+        # TODO remove for loop from test
+        for key in expected_adjacency_dict.keys():
+            np.testing.assert_array_equal(adjacency_dict[key], expected_adjacency_dict[key])
+        
 
 
 if __name__ == '__main__':

program/witness_complex.py
@@ -33,9 +33,19 @@ class Graph:
         if node1.shape != node2.shape:
             return -1
         sum_dist = 0
-        for dim in range(len(node1.shape)):
+        for dim in range(node1.shape[0]):
             sum_dist += (node1[dim] - node2[dim]) * (node1[dim] - node2[dim])
         return np.sqrt(sum_dist)
+
+    def has_node_less_than_k_neighbors(self, node, k):
+        return len(self.adjacency_dict[str(node)]) < k
+
+    def __str__(self):
+        graph_summary = ""
+        graph_summary += "Graph nodes: " + str(self.nodes)
+        graph_summary += "\nGraph adjacency_dict: " + str(self.adjacency_dict)
+        graph_summary += "\nGraph adjacent_nodes_dist: " + str(self.adjacent_nodes_dist)
+        return graph_summary
 
 
 class WitnessComplexCreator:
@@ -45,11 +55,10 @@ class WitnessComplexCreator:
 
     def __define_k_nn(self, k, graph):
         for node1 in graph.nodes:
-            nn = 0
             for node2 in graph.nodes:
                 if graph.are_equal_nodes(node1, node2):
                     continue
-                if len(graph.adjacency_dict[str(node1)]) < k:
+                if graph.has_node_less_than_k_neighbors(node1, k):
                     graph.adjacency_dict[str(node1)].append(node2)
                     graph.adjacent_nodes_dist[str(node1)].append(graph.calculate_distance(node1, node2))
                 else:
@@ -100,8 +109,7 @@ class WitnessComplexCreator:
 
     def create_knn_graph(self, k = 1):
         nodes = uniform_sampler.UniformSampler(list(self.original_input.data)).sample(self.m)
-        # print(nodes)
         graph = Graph(nodes)
         self.__define_k_nn(k, graph)
         self.__augment_knn(self.original_input.data, graph)
-        # print(graph.adjacency_dict)
+        return graph.adjacency_dict